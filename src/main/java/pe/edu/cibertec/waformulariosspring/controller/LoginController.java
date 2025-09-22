22ab1f5042dd8fe5ffcb7a60d2ba1f9edb9ec46c
jeanluismanyari-pixel <jeanluismanyari@gmail.com>
2025-09-21 23:13:36 -0500

    Login controller terminado y diseño terminado

--- src/main/java/pe/edu/cibertec/waformulariosspring/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/waformulariosspring/controller/LoginController.java
@@ -2,13 +2,28 @@ package pe.edu.cibertec.waformulariosspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.cibertec.waformulariosspring.model.LoginModel;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
     public String login(Model model) {
-        model.addAllAttributes("loginModel", new LoginModel());
+        model.addAttribute("loginModel", new LoginModel());
         return "login";
     }
+
+    @PostMapping("/login")
+    public String validarLogin(@ModelAttribute("LoginModel") LoginModel loginModel, Model model) {
+        if (loginModel.getUsuario().equals("lsalvatierra") &&
+                loginModel.getPassword().equals("123")){
+        return "home";
+
+
+    }
+        return "login";
+}
+
 }
