a6ff8a548892d69b477055d9f784093c561c02dd
jeanluismanyari-pixel <jeanluismanyari@gmail.com>
2025-09-21 23:39:05 -0500

    Ejercicio terminado

--- src/main/java/pe/edu/cibertec/waformulariosspring/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/waformulariosspring/controller/LoginController.java
@@ -12,17 +12,21 @@ public class LoginController {
     @GetMapping("/login")
     public String login(Model model) {
         model.addAttribute("loginModel", new LoginModel());
+        model.addAttribute("mostrarMensaje", false);
         return "login";
     }
 
     @PostMapping("/login")
-    public String validarLogin(@ModelAttribute("LoginModel") LoginModel loginModel, Model model) {
+    public String validarLogin(@ModelAttribute("loginModel") LoginModel loginModel, Model model) {
         if (loginModel.getUsuario().equals("lsalvatierra") &&
                 loginModel.getPassword().equals("123")){
+        model.addAttribute("mensaje", "Bienvenido: " + loginModel.getUsuario());
         return "home";
 
 
     }
+        model.addAttribute("mostrarMensaje", true);
+        model.addAttribute( "mensaje", "Usuario y/o password incorrecto");
         return "login";
 }
 
